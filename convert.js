11c9d87c7c69d8bb9000a2c82dc1265229a3f9b7
Andrea Merlin <merlin.andrea@gmail.com>
2018-12-17 21:20:33 +0100

    readmefile

--- convert.js
+++ convert.js
@@ -21,9 +21,3 @@ fileString += 'url(data:font/ttf;base64,' + data2.toString('base64') + ');';
 
 console.log(fileString);
 
-// usare questo
-// node .\convert.js .\karbon-ligth-webfont.woff2 .\karbon-ligth-webfont.woff .\karbon-ligth-webfont.ttf > karbon-ligth.txt
-// node .\convert.js .\karbon-regular-webfont.woff2 .\karbon-regular-webfont.woff .\karbon-regular-webfont.ttf > karbon-regular.txt
-// node .\convert.js .\karbon-medium-webfont.woff2 .\karbon-medium-webfont.woff .\karbon-medium-webfont.ttf > karbon-medium.txt
-// node .\convert.js .\karbon-semibold-webfont.woff2 .\karbon-semibold-webfont.woff .\karbon-semibold-webfont.ttf > karbon-semibold.txt
-// node .\convert.js .\karbon-bold-webfont.woff2 .\karbon-bold-webfont.woff .\karbon-bold-webfont.ttf > karbon-bold.txt
